32cc4ef29668ef42637fde1a4a3c67a514e95a12
ZekariyasGirma <Zekariyas.girma.xy8281@gmail.com>
2024-07-31 12:29:16 +0300

    added a a simple gradient look

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+//import 'package:flutter/widgets.dart';
 import 'package:bahri_app/widgets/CustomStepper.dart';
 import 'package:bahri_app/widgets/LogoCircularBorder.dart';
 
@@ -16,92 +16,108 @@ class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(172, 185, 255, 1),
+      //backgroundColor: const Color.fromRGBO(172, 185, 255, 1),
       body: SafeArea(
-          child: SingleChildScrollView(
-        //physics: const NeverScrollableScrollPhysics(),
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Divider(color: Colors.transparent),
-              const Divider(color: Colors.transparent),
-              const Divider(color: Colors.transparent),
-              FractionallySizedBox(
-                //widthFactor: 0.5,
-                child: Container(
-                  alignment: Alignment.center,
-                  // decoration:
-                  //     BoxDecoration(border: Border.all(color: Colors.black)),
-                  child: const Text(
-                    "Create Account",
-                    style: TextStyle(
-                      fontFamily: "assets/fonts/Poppins-Bold.ttf",
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+          child: Container(
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+              colors: [
+                Color.fromRGBO(183, 153, 255, 1),
+                Color.fromRGBO(172, 188, 255, 1),
+                Color.fromRGBO(174, 226, 255, 1),
+              ]),
+        ),
+        child: SingleChildScrollView(
+          //physics: const NeverScrollableScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Divider(color: Colors.transparent),
+                const Divider(color: Colors.transparent),
+                const Divider(color: Colors.transparent),
+                FractionallySizedBox(
+                  //widthFactor: 0.5,
+                  child: Container(
+                    alignment: Alignment.center,
+                    // decoration:
+                    //     BoxDecoration(border: Border.all(color: Colors.black)),
+                    child: const Text(
+                      "Create Account",
+                      style: TextStyle(
+                        fontFamily: "assets/fonts/Poppins-Bold.ttf",
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const Divider(color: Colors.transparent),
-              const Divider(color: Colors.transparent),
-              const LogoCircularBorder(widthfactor: 0.7),
-              const Divider(color: Colors.transparent),
-              FractionallySizedBox(
-                widthFactor: 0.85,
-                child: CustomStepper(
-                  currentStep: _currentStep,
-                  steps: const [
-                    {'title': 'Email', 'image': 'assets/images/check.svg'},
-                    {'title': 'Name', 'image': 'assets/images/check.svg'},
-                    {'title': 'Birthdate', 'image': 'assets/images/check.svg'},
-                    {'title': 'Gender', 'image': 'assets/images/check.svg'},
-                    {'title': 'Pwd', 'image': 'assets/images/check.svg'},
-                  ],
-                ),
-              ),
-              const Divider(
-                color: Colors.transparent,
-                height: 25,
-              ),
-              FractionallySizedBox(
-                widthFactor: 0.9,
-                child: changeTextBox(_currentStep, context),
-              ),
-              const Divider(
-                color: Colors.transparent,
-                height: 15,
-              ),
-              FractionallySizedBox(
-                widthFactor: 0.9,
-                child: ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _currentStep++;
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFB19EF0),
-                    minimumSize: const Size(double.infinity, 55),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: const Text(
-                    'Continue',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontFamily: "assets/fonts/Poppins-SemiBold.ttf",
-                      //fontWeight: FontWeight.bold,
-                    ),
+                const Divider(color: Colors.transparent),
+                const Divider(color: Colors.transparent),
+                const LogoCircularBorder(widthfactor: 0.7),
+                const Divider(color: Colors.transparent),
+                FractionallySizedBox(
+                  widthFactor: 0.85,
+                  child: CustomStepper(
+                    currentStep: _currentStep,
+                    steps: const [
+                      {'title': 'Email', 'image': 'assets/images/check.svg'},
+                      {'title': 'Name', 'image': 'assets/images/check.svg'},
+                      {
+                        'title': 'Birthdate',
+                        'image': 'assets/images/check.svg'
+                      },
+                      {'title': 'Gender', 'image': 'assets/images/check.svg'},
+                      {'title': 'Pwd', 'image': 'assets/images/check.svg'},
+                    ],
                   ),
                 ),
-              )
-            ],
+                const Divider(
+                  color: Colors.transparent,
+                  height: 25,
+                ),
+                FractionallySizedBox(
+                  widthFactor: 0.9,
+                  child: changeTextBox(_currentStep, context),
+                ),
+                const Divider(
+                  color: Colors.transparent,
+                  height: 15,
+                ),
+                FractionallySizedBox(
+                  widthFactor: 0.9,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _currentStep++;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFB19EF0),
+                      minimumSize: const Size(double.infinity, 55),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: const Text(
+                      'Continue',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontFamily: "assets/fonts/Poppins-SemiBold.ttf",
+                        //fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       )),
