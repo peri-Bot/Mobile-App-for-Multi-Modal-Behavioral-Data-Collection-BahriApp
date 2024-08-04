15fcd921777936c79681566609808f8d01b0e9f1
Roba Daniel <robdan281@gmail.com>
2024-08-04 21:12:30 +0300

    simple fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bahri_app/screens/TeamPages/NoTeamPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -37,7 +38,7 @@ class BahriApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const SignupScreen(),
+      home: const InitialScreen(),
     );
   }
 }
