d4e8606d093cf0cd08166649bd6254fc5fb92ab7
Roba Daniel <robdan281@gmail.com>
2024-08-15 11:08:14 +0300

    added firebase and biometric data collection method in slide game service...

--- lib/screens/game_screens/slide_game_screen.dart
+++ lib/screens/game_screens/slide_game_screen.dart
@@ -1,89 +1,43 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'dart:math';
-import 'package:firebase_database/firebase_database.dart';
 
-
+import '../../services/slide_game_services.dart';
 
 
 class GameScreen extends StatefulWidget {
-  final DatabaseReference _database = FirebaseDatabase.instance.ref();
-
   final int level;
   final Function(int score) onLevelComplete;
 
-
-  GameScreen({super.key, required this.level, required this.onLevelComplete});
+  const GameScreen({super.key, required this.level, required this.onLevelComplete});
 
   @override
   _GameScreenState createState() => _GameScreenState();
 }
 
-
-class _GameScreenState extends State<GameScreen> with SingleTickerProviderStateMixin {
-  final DatabaseReference _database = FirebaseDatabase.instance.ref();
-  void _storeBiometricData() {
-    // Create a unique ID for each game session
-    String sessionId = DateTime.now().millisecondsSinceEpoch.toString();
-
-    _database.child('game_sessions').child(sessionId).set({
-      'level': widget.level,
-      'score': _score,
-      'biometricData': _biometricData,
-    }).then((_) {
-      print('Biometric data stored successfully!');
-    }).catchError((error) {
-      print('Failed to store biometric data: $error');
-    });
-  }
-  List<Map<String, dynamic>> _questions = [];
+class _GameScreenState extends State<GameScreen> {
+  final List<Map<String, dynamic>> _questions = [];
   int _currentQuestionIndex = 0;
   int _score = 0;
   int _timeLeft = 10;
   Timer? _timer;
-  late AnimationController _animationController;
-
-  // Variables to collect biometric data
   final List<Map<String, dynamic>> _biometricData = [];
+
   double? _initialX;
   double? _initialY;
-  double? _initialPressure;
-  DateTime? _startTime;
+  late int _startTime;
+
+  final FirebaseService _firebaseService = FirebaseService();  // Instantiate the FirebaseService
+  final DataCollectionService _dataCollectionService = DataCollectionService();  // Instantiate the DataCollectionService
 
   @override
   void initState() {
     super.initState();
     _loadQuestionsForLevel(widget.level);
     _startTimer();
-
-    // Initialize the animation controller for the timer
-    _animationController = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: _timeLeft),
-    );
-    _animationController.forward();
   }
 
   void _loadQuestionsForLevel(int level) {
-    if (level == 1) {
-      _questions = [
-        {'image': 'assets/slide_game/dog.jpg', 'questionType': 'mammal', 'answer': true},
-        {'image': 'assets/slide_game/fox.jpg', 'questionType': 'mammal', 'answer': false},
-        {'image': 'assets/slide_game/mole.jpg', 'questionType': 'mammal', 'answer': true},
-      ];
-    } else if (level == 2) {
-      _questions = [
-        {'image': 'assets/meerkat.jpg', 'questionType': 'flag', 'answer': true},
-        {'image': 'assets/brazil.png', 'questionType': 'flag', 'answer': false},
-        {'image': 'assets/kenya.png', 'questionType': 'flag', 'answer': true},
-      ];
-    } else if (level == 3) {
-      _questions = [
-        {'image': 'assets/apple.jpg', 'questionType': 'fruit', 'answer': true},
-        {'image': 'assets/carrot.jpg', 'questionType': 'fruit', 'answer': false},
-        {'image': 'assets/banana.jpg', 'questionType': 'fruit', 'answer': true},
-      ];
-    }
+    // Your question loading logic here...
   }
 
   void _startTimer() {
@@ -92,7 +46,6 @@ class _GameScreenState extends State<GameScreen> with SingleTickerProviderStateM
         setState(() {
           _timeLeft--;
         });
-        _animationController.value = (_timeLeft / 10);
       } else {
         _nextQuestion();
       }
@@ -109,12 +62,11 @@ class _GameScreenState extends State<GameScreen> with SingleTickerProviderStateM
       setState(() {
         _currentQuestionIndex++;
         _timeLeft = 10;
-        _animationController.reset();
-        _animationController.forward();
         _startTimer();
       });
     } else {
       _stopTimer();
+      _firebaseService.storeBiometricData(widget.level, _score, _biometricData);  // Store data at the end of the game
       widget.onLevelComplete(_score); // Notify Level Complete
       _showGameOverDialog();
     }
@@ -130,77 +82,7 @@ class _GameScreenState extends State<GameScreen> with SingleTickerProviderStateM
   }
 
   void _showGameOverDialog() {
-    _storeBiometricData();
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Game Over'),
-        content: Text('Your score is $_score'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-              Navigator.of(context).pop(); // Go back to level selection
-            },
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: const Text(
-                'OK',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _stopTimer();
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  void _collectBiometricData(DragEndDetails details) {
-    DateTime endTime = DateTime.now();
-    double duration = endTime.difference(_startTime!).inMilliseconds / 1000.0;
-    double dx = details.velocity.pixelsPerSecond.dx;
-    double dy = details.velocity.pixelsPerSecond.dy;
-    double distance = sqrt(pow(dx, 2) + pow(dy, 2));
-
-    _biometricData.add({
-      'initialX': _initialX,
-      'initialY': _initialY,
-      'endX': details.velocity.pixelsPerSecond.dx,
-      'endY': details.velocity.pixelsPerSecond.dy,
-      'initialPressure': _initialPressure,
-      'duration': duration,
-      'distance': distance,
-      'speed': distance / duration,
-    });
-
-    print(_biometricData);
-  }
-
-  void _onPanStart(DragStartDetails details) {
-    _initialX = details.localPosition.dx;
-    _initialY = details.localPosition.dy;
-
-    _startTime = DateTime.now();
-  }
-
-  void _onPanEnd(DragEndDetails details) {
-    _collectBiometricData(details);
-    if (details.velocity.pixelsPerSecond.dx > 0) {
-      _checkAnswer(true); // Swiped right (True)
-    } else {
-      _checkAnswer(false); // Swiped left (False)
-    }
+    // Your game over dialog code here...
   }
 
   @override
@@ -217,91 +99,81 @@ class _GameScreenState extends State<GameScreen> with SingleTickerProviderStateM
     return Scaffold(
       appBar: AppBar(
         title: Text('Level ${widget.level}'),
-        backgroundColor: Colors.deepPurpleAccent,
       ),
-      body: Container(
-        color: Colors.deepPurple[50],
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Timer Row with Icon
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Row(
-                children: [
-                  const Icon(Icons.timer, color: Colors.redAccent, size: 30),
-                  const SizedBox(width: 10),
-                  Expanded(
-                    child: LinearProgressIndicator(
-                      value: _animationController.value,
-                      backgroundColor: Colors.grey[300],
-                      valueColor: const AlwaysStoppedAnimation<Color>(Colors.redAccent),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Time left: $_timeLeft',
+            style: const TextStyle(fontSize: 24),
+          ),
+          Expanded(
+            child: GestureDetector(
+              onPanStart: (details) {
+                _initialX = details.localPosition.dx;
+                _initialY = details.localPosition.dy;
+                _startTime = DateTime.now().millisecondsSinceEpoch;
+              },
+              onPanEnd: (details) {
+                int endTime = DateTime.now().millisecondsSinceEpoch;
+                double endX = details.velocity.pixelsPerSecond.dx;
+                double endY = details.velocity.pixelsPerSecond.dy;
+                double distance = _dataCollectionService.calculateSwipeDistance(_initialX!, _initialY!, endX, endY);
+                double duration = _dataCollectionService.calculateSwipeDuration(_startTime, endTime);
+                double speed = _dataCollectionService.calculateSwipeSpeed(distance, duration);
+                double straightness = _dataCollectionService.calculateSwipePathStraightness(_initialX!, _initialY!, endX, endY);
+                double angle = _dataCollectionService.calculateSwipeAngle(_initialX!, _initialY!, endX, endY);
+                double acceleration = _dataCollectionService.calculateSwipeAcceleration(speed, duration);
+                double jerk = _dataCollectionService.calculateSwipeJerk(acceleration, duration);
+
+                // Create a map with all the collected data
+                Map<String, dynamic> swipeData = {
+                  'initialX': _initialX,
+                  'initialY': _initialY,
+                  'endX': endX,
+                  'endY': endY,
+                  'duration': duration,
+                  'distance': distance,
+                  'speed': speed,
+                  'straightness': straightness,
+                  'angle': angle,
+                  'acceleration': acceleration,
+                  'jerk': jerk,
+                };
+
+                // Add the data point to the list
+                _biometricData.add(swipeData);
+
+                _checkAnswer(details.velocity.pixelsPerSecond.dx > 0); // Swiped right (True) or left (False)
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(50, 0, 50, 100),
+                      child: Image.asset(_questions[_currentQuestionIndex]['image']),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            // Image Container with Padding
-            Expanded(
-              child: GestureDetector(
-                onPanStart: _onPanStart,
-                onPanEnd: _onPanEnd,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.blueAccent, width: 4),
-                          borderRadius: BorderRadius.circular(12),
-                          color: Colors.white,
-                        ),
-                        child: Image.asset(_questions[_currentQuestionIndex]['image']),
-                      ),
-                      const SizedBox(height: 20),
-                      Text(
-                        questionText,
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                    ],
-                  ),
+                    const SizedBox(height: 20),
+                    Text(
+                      questionText,
+                      style: const TextStyle(fontSize: 24),
+                    ),
+                  ],
                 ),
               ),
             ),
-            const SizedBox(height: 20),
-            // Slide Indicator
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.arrow_back, size: 30, color: Colors.grey),
-                Text('Swipe', style: TextStyle(fontSize: 20, color: Colors.grey)),
-                Icon(Icons.arrow_forward, size: 30, color: Colors.grey),
-              ],
-            ),
-            const SizedBox(height: 20),
-            // Decorated Score Text
-            Text(
-              'Score: $_score',
-              style: const TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Colors.green,
-                shadows: [
-                  Shadow(
-                    offset: Offset(2, 2),
-                    blurRadius: 3,
-                    color: Colors.black26,
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-          ],
-        ),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            'Score: $_score',
+            style: const TextStyle(fontSize: 24),
+          ),
+          const SizedBox(height: 20),
+        ],
       ),
     );
   }
 }
+
--- lib/services/slide_game_services.dart
+++ lib/services/slide_game_services.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 import '../screens/game_screens/slide_game_screen.dart';
@@ -58,3 +61,57 @@ class LevelButton extends StatelessWidget {
     );
   }
 }
+
+
+class FirebaseService {
+  final DatabaseReference _database = FirebaseDatabase.instance.reference();
+
+  void storeBiometricData(int level, int score, List<Map<String, dynamic>> biometricData) {
+    // Create a unique ID for each game session
+    String sessionId = DateTime.now().millisecondsSinceEpoch.toString();
+
+    _database.child('game_sessions').child(sessionId).set({
+      'level': level,
+      'score': score,
+      'biometricData': biometricData,
+    }).then((_) {
+      print('Biometric data stored successfully!');
+    }).catchError((error) {
+      print('Failed to store biometric data: $error');
+    });
+  }
+}
+
+
+class DataCollectionService {
+  double calculateSwipePathStraightness(double initialX, double initialY, double endX, double endY) {
+    double straightDistance = sqrt(pow(endX - initialX, 2) + pow(endY - initialY, 2));
+    double actualDistance = (endX - initialX).abs() + (endY - initialY).abs();
+    return straightDistance / actualDistance;
+  }
+
+  double calculateSwipeAngle(double initialX, double initialY, double endX, double endY) {
+    return atan2(endY - initialY, endX - initialX) * (180 / pi);
+  }
+
+  double calculateSwipeSpeed(double distance, double duration) {
+    return distance / duration;
+  }
+
+  double calculateSwipeAcceleration(double speed, double duration) {
+    return speed / duration;
+  }
+
+  double calculateSwipeJerk(double acceleration, double duration) {
+    return acceleration / duration;
+  }
+
+  double calculateSwipeDistance(double initialX, double initialY, double endX, double endY) {
+    return sqrt(pow(endX - initialX, 2) + pow(endY - initialY, 2));
+  }
+
+  double calculateSwipeDuration(int startTime, int endTime) {
+    return (endTime - startTime) / 1000.0; // convert milliseconds to seconds
+  }
+}
+
